8ff2e564743c5b77990aa3e94e1f6b4097220fb1
Tommaso <tommaso.lodesani@gmail.com>
2022-05-17 17:11:26 +0200

    add log finali

--- script.js
+++ script.js
@@ -1,3 +1,66 @@
+const myScreen = document.getElementById("screen");
+
+const timerNumbers = document.getElementById("timer");
+
+
+
+const arrayNumb = createRandUniqueNumArray(5, 1, 100);
+console.log(arrayNumb);
+
+
+
+myScreen.innerHTML = arrayNumb;
+
+setTimeout(startTimer, 8000);
+
+function startTimer() {
+    myScreen.innerHTML = "";
+
+    let seconds = 10;
+
+    const counting = setInterval(
+
+        function () {
+            timerNumbers.innerHTML = seconds;
+            if (seconds === 0) {
+                clearInterval(counting);
+
+                const arrayUtente = [];
+
+                // let sommaNumeriUtente = 0;
+
+                while (arrayUtente.length < 5) {
+
+                    let numeroUtente = parseInt(prompt("Inserisci numero"));
+                    arrayUtente.push(numeroUtente);
+                    // sommaNumeriUtente += numeroUtente;
+                }
+
+                for(let i = 0; i < arrayUtente.length; i++){
+                    console.log(arrayUtente[i]);
+
+                    let arrayUteItems = arrayUtente[i];
+
+                    if(arrayNumb.includes(arrayUteItems)){
+                        console.log("numero trovato");
+                    } else {
+                        console.log("numero non trovato");
+                    }
+                }
+                
+
+
+                console.log(arrayUtente);
+            } else {
+                seconds--;
+            }
+        }
+
+        , 1000)
+}
+
+
+
 
 
 
